4a57b406a365b096135dcbda0e6861b1e76a8ce4
Megha Raj <megha.nambiar@gmail.com>
2022-03-05 20:19:11 -0800

    Added https to api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var getCityDetails = function(city)
     //This api will take the city string and return the latitude/longitude. This will allow the user to input a city name instead of location details
     //Geo details are required for the api with forecast data. Limiting data returned to 1 record.
     //
-    var geoCodeUrl= "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+api_key;
+    var geoCodeUrl= "https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+api_key;
 
     fetch(geoCodeUrl)
     .then ( function(response){
