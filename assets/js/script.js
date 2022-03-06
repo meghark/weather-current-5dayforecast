503bf2d5eee808c7f29bbe215baa139f08f1ddf6
Megha Raj <megha.nambiar@gmail.com>
2022-03-05 20:11:55 -0800

    Added code to print forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,17 @@ var getCityDetails = function(city)
 }
 
 var getWeatherData = function(){
-    //This api call will use the latitude and longitude to return current and forecast data
-    var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+api_key+"&units=imperial";
+    //This api call will use the latitude and longitude to return current and forecast data.
+    //The web page displays only the current and  daily data. So the api call includes the parameter to exclude minutely, hourly , alerts etc.
+    //Fetch only the rquired information.
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+api_key+"&units=imperial&exclude=minutely,hourly,alerts";
 
     fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
+        console.log(data);
         //Call the following function to display the current weather. Only the current weather details are passed to the function.
         printCurrentWeather(data.current);
 
@@ -63,7 +66,36 @@ var getWeatherData = function(){
  }
 
  var printForecastWeather = function(data){
-    console.log(data);
+        var forecastHeaderEl= $(".header-forecast");
+        var headerEl = $("<h2>").text("5-Day Forecast:");
+        forecastHeaderEl.append(headerEl);
+
+        var forecastEl = $(".forecast") ;
+        for (var i =0 ; i<5; i++)
+        {
+            var current = data[i];
+            var getDate = new Date(current.dt * 1000);  
+            
+            var divCardEl = $("<div>")
+                            .addClass("pure-u-1 pure-u-md-1-5");
+            
+            var ulEl = $("<ul>")
+                        .addClass("weatherCard");
+              
+            var dateEl = $("<li>")
+                        .addClass("day")
+                        .text(getDate.toLocaleDateString());
+            var tempEl = $("<li>")
+                        .text(current.temp.max);
+            var windEl = $("<li>")
+                        .text(current.wind_speed+" Mph");
+            var humidityEl = $("<li>")
+                        .text(current.humidity+" %");
+
+            ulEl.append(dateEl, tempEl, windEl, humidityEl);
+            divCardEl.append(ulEl);
+            forecastEl.append(divCardEl);
+        }
  }
 
 getCityDetails(city);
